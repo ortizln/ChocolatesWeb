110c71d9eee79018b1c1c7f2ea6dd91374b69bee
ortizln <ortizln9@gmail.com>
2026-06-24 16:22:18 -0500

    update

--- frontend/public-site/src/environments/environment.ts
+++ frontend/public-site/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.100.215:8082/api/v1',
-  siteUrl: 'http://192.168.100.215:4200'
+  apiUrl: 'http://localhost:8082/api/v1',
+  siteUrl: 'http://localhost:4200'
 };
